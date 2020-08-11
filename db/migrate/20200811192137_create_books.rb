19007596cc44cd32a0355daff24330d1741dcd7f
koobcbc <kook@wra.net>
2020-08-12 04:53:39 +0900

    Created Schema and Set up ActiveRecord Relations

--- db/migrate/20200811192137_create_books.rb
+++ db/migrate/20200811192137_create_books.rb
@@ -2,19 +2,19 @@ class CreateBooks < ActiveRecord::Migration[6.0]
   def change
     create_table :books do |t|
       t.string :title
-      t.string[] :authors array: true, default: []
+      t.string :authors, array: true, default: []
       t.string :year
       t.string :publisher
-      t.string[] :imageLinks
+      t.string :imageLinks, array: true, default: []
       t.integer :pageNumber
       t.text :description
       t.string :language
-      t.string[] :categories
+      t.string :categories, array: true, default: []
       t.string :canonicalVolumeLink
       t.decimal :starRating
-      t.string[] :reviews
-      t.string[] :notes
-      t.string[] :quotes
+      t.string :reviews, array: true, default: []
+      t.string :notes, array: true, default: []
+      t.string :quotes, array: true, default: []
 
       t.timestamps
     end
