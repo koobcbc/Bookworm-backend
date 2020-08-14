5995f12667f710304348dde0eb75a1bf964691db
koobcbc <kook@wra.net>
2020-08-15 05:15:08 +0900

    configured cors

--- bookworm_app_api/config/routes.rb
+++ bookworm_app_api/config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 
   resources :users do                                                            
     collection do                                                                
-      post '/login', to: 'users#login'                                            
+      post '/login', to: 'users#login'
+      get '/auto_login', to: 'users#auto_login'                                  
     end                                                                          
   end       
                                 
